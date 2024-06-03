ba27dbd6435afdeb1bb248e8753df8dc0479d2f7
vlad solo <vlad.good47@mail.ru>
2024-06-03 21:25:23 +0300

    creating style AIM-Game

---
+++ style.js
@@ -0,0 +1,8 @@
+const start = document.getElementById('start');
+const cardS = document.getElementById('card');
+const timeController = document.addEventListener('.time-list');
+const modal = document.addEventListener('.modal');
+const board = document.getElementById('board');
+const time = document.getElementById('time');
+const score = document.getElementById('score');
+
