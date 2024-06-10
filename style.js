c081440af2d5227f5bc02ca759b94ee3a5b710af
vlad solo <vlad.good47@mail.ru>
2024-06-10 21:19:05 +0300

    creating game

--- style.js
+++ style.js
@@ -4,15 +4,26 @@ const timeController = document.getElementById('time-list');
 const modal = document.getElementById('modal');
 const board = document.getElementById('board');
 const timeEl = document.getElementById('time');
-const score = document.getElementById('score');
+const scoreEl = document.getElementById('score');
 
 let selectedTime = null
 let time = null
 let idSetInterval = null
+let score = 0
 
 function handlerStartBtn (e) {
     e.preventDefault()
     cards[0].classList.add('up')
+
+    // const newDiv = document.createElement('div');
+    // newDiv.innerHTML = 'Hello World';
+    // start.prepend(newDiv);
+
+    // const myEl = document.createElement('div');
+    // const currentTime = new Date().toISOString()
+    // myEl.textContent = currentTime
+    // cards[0].insertAdjacentElement('beforeend', myEl);
+
 }
 start.addEventListener('click', handlerStartBtn)
 
@@ -28,21 +39,52 @@ function handlerTimeBtn (e) {
 }
 timeController.addEventListener('click', handlerTimeBtn)
 
+function handlerCircleClick(e) {
+    console.log('click');
+    if (e.target.classList.contains('circle')) {
+        score = score + 1 // score++
+        e.target.remove()
+        createRandomCircle()
+    }
+}
+
+board.addEventListener('click', handlerCircleClick)
+
+function createRandomCircle() {
+    const circle = document.createElement('div')
+    circle.classList.add('circle')
+    const size = getRandomIntInclusive(15, 50)
+    const {width, height} = board.getBoundingClientRect()
+    circle.style.width = circle.style.height = size + 'px'
+    circle.style.backgroundColor = getRandomColor()
+    const x = getRandomIntInclusive(0, width - size)
+    const y = getRandomIntInclusive(0, height - size)
+    circle.style.top = y +'px'
+    circle.style.left = x +'px'
+    board.append(circle)
+}
+
+function getRandomColor() {
+  return `rgb(${getRandomIntInclusive(0, 255)},${getRandomIntInclusive(0, 255)},${getRandomIntInclusive(0, 255)})`
+}
+
 function startGame() {
     setTime(time)
     idSetInterval = setInterval(decTime, 1000)
     console.log(idSetInterval);
+    createRandomCircle()
 }
 
 function finishGame() {
     console.log('GameOver');
+    scoreEl.innerHTML = score
+    modal.classList.add('open')
     clearInterval(idSetInterval)
 }
 
 
 function decTime(e) {
     if (time === 0) {
-        console.log('game over');
         finishGame()
     } else {
         let current = --time // time - 1
@@ -61,3 +103,31 @@ function setTime(timeGame) {
     timeEl.innerHTML = `00:${timeGame}`
 }
 
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  }
+
+  modal.addEventListener('click', handlerModalClick)
+
+function resetGame() {
+    this.classList.remove('open')
+    time = selectedTime
+    score = 0
+    board.innerHTML = ''
+}
+
+  function handlerModalClick(e) {
+    if (e.target.getAttribute('id') === 'restart') {
+        console.log('restart');
+        resetGame.call(this)
+        startGame()
+    }
+    if (e.target.getAttribute('id') === 'cancel') {
+        resetGame.call(this)
+        cards.forEach((card) => {
+            card.classList.remove('up')
+        })
+    }
+  }
